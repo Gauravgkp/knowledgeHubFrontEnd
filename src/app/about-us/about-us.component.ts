52f9889e86599387cb1b0b1aba4af05bf8f62b98
Komal_R <komalr984@gmail.com>
2019-03-15 16:12:05 +0530

    About us completed

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.css','/lib/font-awesome/css/font-awesome.min.css',
-  '/lib/animate/animate.min.css','/lib/ionicons/css/ionicons.min.css',
-  '/lib/owlcarousel/assets/owl.carousel.min.css','/lib/magnific-popup/magnific-popup.css',
-'/lib/ionicons/css/ionicons.min.css','/lib/bootstrap/css/bootstrap.min.css']
+  styleUrls: ['./about-us.component.css','../../assets/aboutUslib/font-awesome/css/font-awesome.min.css',
+  '../../assets/aboutUslib/animate/animate.min.css','../../assets/aboutUslib/ionicons/css/ionicons.min.css',
+  '../../assets/aboutUslib/owlcarousel/assets/owl.carousel.min.css','../../assets/aboutUslib/magnific-popup/magnific-popup.css',
+'../../assets/aboutUslib/ionicons/css/ionicons.min.css','../../assets/aboutUslib/bootstrap/css/bootstrap.min.css']
 })
 export class AboutUsComponent implements OnInit {
 
