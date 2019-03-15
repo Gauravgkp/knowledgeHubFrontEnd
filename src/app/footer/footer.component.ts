4e825f69e3eac56ef0052fa555eeb17b5dc0f018
uday n <udayn612@gmail.com>
2019-03-15 15:53:03 +0530

    footer component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css','../../assets/lib/font-awesome/css/font-awesome.min.css',
+'../../assets/lib/bootstrap/css/bootstrap.min.css']
 })
 export class FooterComponent implements OnInit {
 
