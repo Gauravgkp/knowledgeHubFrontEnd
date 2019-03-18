c681032d28fa05b3213d805b5c08faf9f21ac1a9
venkatanagasairam.sakhamuri <venkatanagasairam.sakhamuri@boeing.com>
2019-03-18 14:55:29 +0530

    added jwt token and login component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css','../../assets/conceptsLib/font-awesome/css/font-awesome.min.css',
+  '../../assets/conceptsLib/animate/animate.min.css','../../assets/conceptsLib/ionicons/css/ionicons.min.css',
+  '../../assets/conceptsLib/owlcarousel/assets/owl.carousel.min.css','../../assets/conceptsLib/magnific-popup/magnific-popup.css',
+'../../assets/conceptsLib/ionicons/css/ionicons.min.css','../../assets/conceptsLib/bootstrap/css/bootstrap.min.css']
 })
 export class HomeComponent implements OnInit {
 
