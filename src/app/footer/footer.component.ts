718481e64beaf77a6097cc2cc8fd322fbb64cd0a
Meghana_N <meghanangwd@gmail.com>
2019-03-18 15:19:00 +0530

    added footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,8 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css','../../assets/lib/font-awesome/css/font-awesome.min.css',
-'../../assets/lib/bootstrap/css/bootstrap.min.css']
+  styleUrls: ['./footer.component.css',
+  '../../assets/footer/css/bootstrap.min.css',
+  '../../assets/footer/css/font-awesome.min.css',
+  '../../assets/footer/css/animate.min.css',
+  '../../assets/footer/css/prettyPhoto.css',
+  '../../assets/footer/css/style.css',
+  '../../assets/footer/css/responsive.css'
+
+// <link href="css/bootstrap.min.css" rel="stylesheet">
+//   <link rel="stylesheet" href="css/font-awesome.min.css">
+//   <link href="css/animate.min.css" rel="stylesheet">
+//   <link href="css/prettyPhoto.css" rel="stylesheet">
+//   <link href="css/style.css" rel="stylesheet">
+//   <link href="css/responsive.css" rel="stylesheet">
+]
 })
 export class FooterComponent implements OnInit {
 
